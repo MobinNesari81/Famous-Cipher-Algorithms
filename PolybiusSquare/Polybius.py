6a8ed0f8aacffadba658c0bd0cc4b61bfa4d9bd0
Mobin Nesari <mobinnesari81@gmail.com>
2023-03-28 18:09:09 +0330

    Add comments to Polybius.py

--- PolybiusSquare/Polybius.py
+++ PolybiusSquare/Polybius.py
@@ -1,5 +1,9 @@
 class PolybiusSquareCipher:
     def __init__(self):
+        """
+        Initializes an instance of the PolybiusSquareCipher class with a 5x5 square
+        containing the letters A to Z except J where I is used instead.
+        """
         self.alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
         self.square = [["A", "B", "C", "D", "E"],
                        ["F", "G", "H", "I", "K"],
@@ -7,7 +11,16 @@ class PolybiusSquareCipher:
                        ["Q", "R", "S", "T", "U"],
                        ["V", "W", "X", "Y", "Z"]]
     
-    def encrypt(self, plaintext):
+    def encrypt(self, plaintext: str) -> str:
+        """
+        Encrypts the given plaintext using the Polybius square cipher.
+
+        Parameters:
+        plaintext (str): The text to be encrypted.
+
+        Returns:
+        str: The encrypted ciphertext.
+        """
         ciphertext = ""
         for char in plaintext.upper():
             if char == "J":
@@ -17,7 +30,16 @@ class PolybiusSquareCipher:
                 ciphertext += str(row) + str(col)
         return ciphertext
     
-    def decrypt(self, ciphertext):
+    def decrypt(self, ciphertext: str) -> str:
+        """
+        Decrypts the given ciphertext using the Polybius square cipher.
+
+        Parameters:
+        ciphertext (str): The text to be decrypted.
+
+        Returns:
+        str: The decrypted plaintext.
+        """
         plaintext = ""
         i = 0
         while i < len(ciphertext):
@@ -27,17 +49,17 @@ class PolybiusSquareCipher:
             i += 2
         return plaintext
     
-    def find_char(self, char):
+    def find_char(self, char: str) -> tuple:
+        """
+        Finds the row and column of a given character in the Polybius square.
+
+        Parameters:
+        char (str): The character to be found in the square.
+
+        Returns:
+        tuple: A tuple containing the row and column indices of the character.
+        """
         for i in range(len(self.square)):
             for j in range(len(self.square[i])):
                 if self.square[i][j] == char:
                     return i+1, j+1
-
-
-if __name__ == "__main__":
-    PSC = PolybiusSquareCipher()
-    text = "I will see you at midnight"
-    enc = PSC.encrypt(text)
-    print(enc)
-    dec = PSC.decrypt(enc)
-    print(f" Text: {text}\n Ciphertext: {enc}\n Decrypted: {dec}")
